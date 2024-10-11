d443bb375f6cc2a4695a6687f6ea66fc4c0c102f
AniketSharma-dev <aniketsharma.xo@gmail.com>
2024-10-12 02:34:15 +0530

    Added file

--- app.js
+++ app.js
@@ -78,55 +78,87 @@ function elements() {
   });
 }
 
-var arr = [
-  {
-    name: "Emma",
-    dp: "https://images.unsplash.com/photo-1528962728833-5106fc29b9c8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    story:
-      "https://images.unsplash.com/photo-1727731481816-bf055858e20f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Olivia",
-    dp: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    story:
-      "https://images.unsplash.com/photo-1533618561606-3b2a0766d159?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Ava",
-    dp: "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?q=80&w=1727&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    story:
-      "https://plus.unsplash.com/premium_photo-1664046912334-134dbd487444?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Sophia",
-    dp: "https://images.unsplash.com/photo-1482555670981-4de159d8553b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    story:
-      "https://images.unsplash.com/photo-1527285286036-1ae743926077?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Isabella",
-    dp: "https://images.unsplash.com/photo-1512310604669-443f26c35f52?q=80&w=1934&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    story:
-      "https://images.unsplash.com/photo-1698620625458-340dba2af9ea?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-];
+function story() {
+  var arr = [
+    {
+      name: "Emma",
+      dp: "https://images.unsplash.com/photo-1528962728833-5106fc29b9c8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      story:
+        "https://images.unsplash.com/photo-1727731481816-bf055858e20f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "Olivia",
+      dp: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      story:
+        "https://images.unsplash.com/photo-1533618561606-3b2a0766d159?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "Ava",
+      dp: "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?q=80&w=1727&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      story:
+        "https://plus.unsplash.com/premium_photo-1664046912334-134dbd487444?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "Sophia",
+      dp: "https://images.unsplash.com/photo-1482555670981-4de159d8553b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      story:
+        "https://images.unsplash.com/photo-1527285286036-1ae743926077?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "Isabella",
+      dp: "https://images.unsplash.com/photo-1512310604669-443f26c35f52?q=80&w=1934&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      story:
+        "https://images.unsplash.com/photo-1698620625458-340dba2af9ea?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
 
-var storySection = document.querySelector(".main-screen");
-var cluster = "";
+  var storySection = document.querySelector(".main-screen");
+  var fullScreen = document.querySelector(".full-screen");
+  var cluster = "";
 
-arr.forEach(function (e, index) {
-  cluster += ` <div class="story">
-    <img id="${index}" src="${e.dp}" alt="${e.name}">
+  arr.forEach(function (e, index) {
+    cluster += ` <div class="story">
+    <img data-index="${index}" src="${e.dp}" alt="${e.name}">
   </div>`;
-});
+  });
 
-storySection.innerHTML = cluster;
+  storySection.innerHTML = cluster;
 
-storySection.addEventListener("click", function(e){
-  console.log(e.target);
-  
-})
+  storySection.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
+      // Check if you clicked at is image or not
+      var index = e.target.getAttribute("data-index"); // Get the number (data-index) of the image you clicked.
+      var value = arr[index].story; // Use that index to find the correct story.
 
+      // Start the progress bar
+      var progressBar = document.querySelector(".progress-bar");
+      progressBar.style.transition = "none"; // Temporarily disable the transition
+      progressBar.style.width = "0";
+
+      setTimeout(function () {
+        progressBar.style.transition = "width 3s linear"; // Re-enable the transition
+        progressBar.style.width = "100%"; // Animate the progress bar to 100% over 3 seconds
+      }, 50);
+
+      fullScreen.style.backgroundImage = `url(${value})`; // Set the background image as the story.
+      fullScreen.style.display = "block"; // Show story.
+
+      autoClose = setTimeout(function () {
+        fullScreen.style.display = "none";
+        document.querySelector(".progress-bar").style.width = "0"; // Reset progress bar
+      }, 3000);
+    }
+  });
+
+  var closeButton = document.querySelector(".close-btn");
+
+  closeButton.addEventListener("click", () => {
+    fullScreen.style.display = "none";
+    clearTimeout(autoClose);
+  });
+}
+
+story();
 elements();
 customCursor();
 heart();
