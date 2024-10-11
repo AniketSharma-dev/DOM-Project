6ad7a82593d72babf3b91a3a37ca1db9200362bf
AniketSharma-dev <aniketsharma.xo@gmail.com>
2024-10-12 00:02:57 +0530

    Added file

--- app.js
+++ app.js
@@ -38,24 +38,53 @@ function heart() {
   });
 }
 
-var main = document.querySelector("body");
-var crsr = document.querySelector(".cursor");
+function customCursor() {
+  var main = document.querySelector("body");
+  var crsr = document.querySelector(".cursor");
 
-main.addEventListener("mousemove", (e) => {
-  crsr.style.left = e.clientX + "px";
-  crsr.style.top = e.clientY + "px";
+  main.addEventListener("mousemove", (e) => {
+    crsr.style.left = e.clientX + "px";
+    crsr.style.top = e.clientY + "px";
 
+    /* pageX and pageY use for page event like it only follow page area but in client it follow client screen  */
+    // crsr.style.left = e.pageX + "px";
+    // crsr.style.top = e.pageY + "px";
+  });
 
+  window.addEventListener("load", () => {
+    crsr.style.left = "-50px"; // Hide the cursor initially off-screen
+    crsr.style.top = "-50px";
+  });
+}
 
-  /* pageX and pageY use for page event like it only follow page area but in client it follow client screen  */
-  // crsr.style.left = e.pageX + "px";
-  // crsr.style.top = e.pageY + "px";
+var elem = document.querySelectorAll(".elem");
+
+elem.forEach(function (val) {
+  val.addEventListener("mouseenter", function () {
+    val.style.cursor = "pointer";
+    console.log(val.childNodes[3]);
+
+    val.childNodes[3].style.opacity = 1;
+  });
+  val.addEventListener("mouseleave", function () {
+    val.childNodes[3].style.opacity = 0;
+  });
 });
 
-window.addEventListener("load", () => {
-  crsr.style.left = "-50px"; // Hide the cursor initially off-screen
-  crsr.style.top = "-50px";
-});
+// var elem = document.querySelector(".elem");
+// var image = document.querySelector(".img");
 
+// elem.addEventListener("mousemove", function (e) {
+//   console.log(e);
+
+//   image.style.left = e.x + "px";
+//   image.style.top = e.y + "px";
+//   image.style.opacity = 1;
+// });
+// elem.addEventListener("mouseleave", function () {
+//   image.style.opacity = 0;
+// });
+
+// customCursor();
 heart();
 addFriend();
